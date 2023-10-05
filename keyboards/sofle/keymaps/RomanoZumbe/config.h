c334be97ff7db464da525d6c3aa5aec94c1f9a29
Romano Zumbe <rzumbe@gmail.com>
2023-10-05 11:26:05 +0200

    macros

---
+++ keyboards/sofle/keymaps/RomanoZumbe/config.h
@@ -0,0 +1,25 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+#define UNICODE_SELECTED_MODES UNICODE_MODE_WINCOMPOSE
+#define MAIN_BOARD
+#define MASTER_LEFT
+#define OLED_TIMEOUT 10000
+
+#define MOUSEKEY_INTERVAL 16
+#define MOUSEKEY_DELAY 0
+#define MOUSEKEY_TIME_TO_MAX 60
+#define MOUSEKEY_MAX_SPEED 7
+#define MOUSEKEY_WHEEL_DELAY 0
+
+#ifdef TAPPING_TERM
+#    undef TAPPING_TERM
+#endif
+#define TAPPING_TERM 200
+
+#undef MANUFACTURER
+#define MANUFACTURER "Romano"
